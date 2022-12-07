3ba3f6d9f58cc45159696370a938e1374464333f
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-07 17:29:12 +0200

    FROM 1-13

--- milestone2.sql
+++ milestone2.sql
@@ -7,41 +7,41 @@ CREATE PROC createAllTables AS
 
 	CREATE TABLE Stadium (
 		id int Primary Key Identity,
-		sname VARCHAR(60),
-		slocation VARCHAR(60),
+		sname VARCHAR(20),
+		slocation VARCHAR(20),
 		capacity int,
 		is_available BIT,
 	)
 
 	CREATE TABLE Club (
 		id int Primary Key Identity,
-		clocation VARCHAR(60),
-		cname VARCHAR(60),
+		cname VARCHAR(20),
+		clocation VARCHAR(20),
 	)
 
 	CREATE TABLE Game (
 		id int Primary Key Identity,
+		f_club VARCHAR(20) FOREIGN KEY REFERENCES Club(id),
+		s_club VARCHAR(20) FOREIGN KEY REFERENCES Club(id),
+		h_club VARCHAR(20),
 		starting_time date,
-		end_time date,
-		allowed_attendees int,
+--		end_time date,
 		stadium_id int FOREIGN KEY REFERENCES Stadium(id),
-		guest_id int FOREIGN KEY REFERENCES Club(id),
-		hoster_id int FOREIGN KEY REFERENCES Club(id)
 	)
 
 	CREATE TABLE SystemUser (
 		id int Primary Key Identity,
-		username VARCHAR(60),
-		uname VARCHAR(60),
-		upassword VARCHAR(60),
-		urole VARCHAR(60),
+		uname VARCHAR(20),
+		username VARCHAR(20),
+		upassword VARCHAR(20),
+		urole VARCHAR(20),
 	)	
 
 	CREATE TABLE Fan (
-		id int Primary Key Identity,
+		id int Primary Key,
 		phone_number int,
 		birth_date Date,
-		fan_address VARCHAR(60),
+		fan_address VARCHAR(100),
 		is_blocked BIT,
 		FOREIGN KEY (id) REFERENCES SystemUser(id)
 		)
@@ -70,17 +70,12 @@ CREATE PROC createAllTables AS
 
 	CREATE TABLE Ticket (
 		id int Primary Key Identity,
-		is_available BIT,
-		fan_id int FOREIGN KEY REFERENCES Fan(id),
-	)
-
-	CREATE TABLE Game_Ticket (
 		game_id int FOREIGN KEY REFERENCES Game(id),
-		ticket_id int FOREIGN KEY REFERENCES Ticket(id),
-		Primary KEY (game_id, ticket_id)
+		fan_id int FOREIGN KEY REFERENCES Fan(id),
+		is_available BIT,
 	)
 
-	CREATE TABLE Permission (
+	CREATE TABLE Request (
 		id int Primary Key,
 		is_approved BIT,
 		game_id int FOREIGN KEY REFERENCES Game(id),
@@ -92,7 +87,7 @@ CREATE PROC createAllTables AS
 --EXEC createAllTables;
 
 CREATE PROC dropAllTables AS
-	DROP TABLE IF EXISTS Permission, Game_Ticket, Ticket, System_Admin,
+	DROP TABLE IF EXISTS Request, Ticket, System_Admin,
 	Representative, Sports_Association_Manager, Manager,
 	Fan, SystemUser, Game, Club, Stadium;
 GO
@@ -116,8 +111,7 @@ CREATE PROC dropAllProceduresFunctionsViews AS
 	GO
 
 CREATE PROC clearAllTables AS
-	TRUNCATE TABLE Permission;
-	TRUNCATE TABLE Game_Ticket;
+    TRUNCATE TABLE Request;
 	TRUNCATE TABLE System_Admin;
 	TRUNCATE TABLE Ticket;
 	TRUNCATE TABLE Sports_Association_Manager;
@@ -130,3 +124,59 @@ CREATE PROC clearAllTables AS
 	TRUNCATE TABLE Stadium;
 	GO
 
+CREATE VIEW allAssocManagers AS
+    SELECT SU.username, SU.uname
+    FROM Sports_Association_Manager SAM
+    JOIN SystemUser SU ON SU.id = SAM.id;
+GO
+
+CREATE VIEW allClubRepresentatives AS
+    SELECT su.username, su.uname, c.cname
+    FROM Representative R
+    JOIN SystemUser SU ON SU.id = R.id
+	JOIN Club C ON C.id = r.club_id;
+GO
+
+CREATE VIEW allStadiumManagers AS
+    SELECT su.username, su.uname, S.sname
+    FROM Manager M
+    JOIN SystemUser SU ON SU.id = M.id
+	JOIN Stadium S ON S.id = M.stadium_id;
+GO
+
+CREATE VIEW allFans AS
+    SELECT SU.uname, F.id, F.birth_date, F.is_blocked
+    FROM Fan F
+    JOIN SystemUser SU ON SU.id = F.id;
+GO
+
+CREATE VIEW allMatches AS
+    SELECT f_club, s_club, host_club, starting_time
+    FROM Game G
+GO
+
+CREATE VIEW allTickets AS
+    SELECT g.f_club, g.s_club, S.sname, g.starting_time
+    FROM Ticket T
+	JOIN Game G ON T.game_id = G.id
+	JOIN Stadium S ON S.id = G.stadium_id;
+GO
+
+CREATE VIEW allCLubs AS
+    SELECT cname, clocation
+    FROM Club;
+GO
+
+CREATE VIEW allStadiums AS
+    SELECT sname, slocation, capacity, is_available
+    FROM Stadium S;
+GO
+
+CREATE VIEW allRequests AS
+    SELECT SU1.uname, SU2.uname,P.is_approved
+    FROM Request Re
+	JOIN Representative R ON R.representative_id = Re.id
+	JOIN SystemUser SU1 ON SU1.id = R.id
+	JOIN Manager M ON RE.manager_id = M.id
+	JOIN SystemUser SU2 ON SU2.id = P.id;
+GO

c4973dda136e15f47171c2bfffbd02bab982fe79
Abdulrahman Fahmy <75482475+abdulrhman500@users.noreply.github.com>
2022-12-14 02:43:53 +0200

    Add files via upload

--- milestone2.sql
+++ milestone2.sql
@@ -6,115 +6,158 @@ EXEC dropAllProceduresFunctionsViews;
 DROP PROC dropAllProceduresFunctionsViews;
 */
 
-
-
 --CREATE DATABASE DBProject;
 --USE DBProject;
 
 GO
 CREATE PROC createAllTables AS
 
+	CREATE TABLE SystemUser (
+		username VARCHAR(20),
+		name VARCHAR(20),
+		password VARCHAR(20),
+
+		constraint SyU_PK primary key(username)
+	)	
+
 	CREATE TABLE Stadium (
-		id int Primary Key Identity,
-		sname VARCHAR(20) UNIQUE,
-		slocation VARCHAR(20),
+		id int Identity,
+		name VARCHAR(20) UNIQUE,
+		location VARCHAR(20),
 		capacity int,
-		is_available BIT DEFAULT 1
+		is_available BIT DEFAULT 1,
+
+		constraint St_PK primary key(id)
 	)
 
 	CREATE TABLE Club (
-		id int Primary Key Identity,
-		cname VARCHAR(20) UNIQUE,
-		clocation VARCHAR(20),
+		id int Identity,
+		name VARCHAR(20) UNIQUE,
+		location VARCHAR(20),
+
+		constraint cl_PK primary key(id)
 	)
 
-	CREATE TABLE Game (
-		id int Primary Key Identity,
-		f_club VARCHAR(20) FOREIGN KEY REFERENCES Club(cname),
-		s_club VARCHAR(20) FOREIGN KEY REFERENCES Club(cname),
-		h_club VARCHAR(20) FOREIGN KEY REFERENCES Club(cname),
+	CREATE TABLE Match (
+		id int Identity,
+		guest_club int,
+		host_club int ,
 		starting_time datetime,
---		end_time date,
-		sname VARCHAR(20) FOREIGN KEY REFERENCES Stadium(sname) ON UPDATE CASCADE,
-	)
+		end_time datetime,
+		stadium_id int ,
+	
+		constraint Ma_PK primary key (id),
+		constraint Ma_GC_FK FOREIGN KEY(guest_club) REFERENCES Club(id) on update cascade ,
+		constraint Ma_HC_FK FOREIGN KEY(host_club) REFERENCES Club(id) on delete set null ,
+		constraint Ma_si_FK FOREIGN KEY(stadium_id) REFERENCES stadium(id) on update cascade on delete set null
 
-	CREATE TABLE SystemUser (
-		id int Primary Key Identity,
-		uname VARCHAR(20),
-		username VARCHAR(20),
-		upassword VARCHAR(20),
-	)	
+	)
+	
+
 
 	CREATE TABLE Fan (
-		id int Primary Key,
-		phone_number int,
+		National_id varchar(20),
+		username varchar(20),
+		phone_no int,
 		birth_date Date,
-		fan_address VARCHAR(100),
+		fan_address VARCHAR(20),
 		is_blocked BIT DEFAULT 0,
-		FOREIGN KEY (id) REFERENCES SystemUser(id)
-		ON DELETE CASCADE ON UPDATE CASCADE,
+		constraint Fa_PK primary  KEY (National_id),
+		constraint Fa_un_Fk Foreign key (username) REFERENCES SystemUser (username) on delete set null
 		)
 	
-	CREATE TABLE Manager (
-		id int Primary Key,
-		sname VARCHAR(20) FOREIGN KEY REFERENCES Stadium(sname)
-		ON DELETE CASCADE ON UPDATE CASCADE,
-		FOREIGN KEY (id) REFERENCES SystemUser(id)
-		ON DELETE CASCADE ON UPDATE CASCADE
+	CREATE TABLE Stadium_Manager (
+		id int identity ,
+		username varchar(20),
+		stadium_id int ,
+
+		constraint sm_PK primary key (id),
+		constraint sm_un_FK Foreign key (username) references SystemUser(username) ON DELETE CASCADE ON UPDATE CASCADE,
+		constraint sm_si_FK Foreign key (stadium_id) references stadium(id) ON DELETE CASCADE ON UPDATE CASCADE
+
 	)
 
-	CREATE TABLE Representative (
-		id int Primary Key,
-		cname VARCHAR(20) FOREIGN KEY REFERENCES Club(cname)
-		ON DELETE CASCADE ON UPDATE CASCADE,
-		FOREIGN KEY (id) REFERENCES SystemUser(id)
-		ON DELETE CASCADE ON UPDATE CASCADE
+	CREATE TABLE Club_Representative (
+		id int identity,
+		username varchar(20),
+		club_id int ,
+		
+		constraint sm_PK primary key (id),
+		constraint sm_un_FK Foreign key (username) references SystemUser(username) ON DELETE CASCADE ON UPDATE CASCADE,
+		constraint sm_ci_FK Foreign key (club_id) references club(id) ON DELETE CASCADE ON UPDATE CASCADE
+
 	)
 
 	CREATE TABLE Sports_Association_Manager (
-		id int Primary Key,
-		FOREIGN KEY (id) REFERENCES SystemUser(id)
-		ON DELETE CASCADE ON UPDATE CASCADE
+		id int identity ,
+		username varchar(20),
+
+		constraint SAM_PK primary key (id),
+		constraint SAM_un_FK Foreign key (username) references SystemUser(username) ON DELETE CASCADE ON UPDATE CASCADE
+	
 	)
 
 	CREATE TABLE System_Admin (
-		id int Primary Key,
-		FOREIGN KEY (id) REFERENCES SystemUser(id)
-		ON DELETE CASCADE ON UPDATE CASCADE
+		id int identity ,
+		username varchar(20),
+
+		constraint SA_PK primary key (id),
+		constraint SA_un_FK Foreign key (username) references SystemUser(username) ON DELETE CASCADE ON UPDATE CASCADE
+	
 	)
 
 	CREATE TABLE Ticket (
-		id int Primary Key Identity,
-		game_id int FOREIGN KEY REFERENCES Game(id) ON DELETE CASCADE,
-		fan_id int FOREIGN KEY REFERENCES Fan(id) ON UPDATE CASCADE,
-		is_available BIT,
+		id int Identity,
+		Match_id int ,
+		is_available BIT ,
+	
+	constraint Ti_PK primary key(id),
+	constraint Ti_mi_FK foreign key(Match_id) references Match(id) on update cascade ON DELETE CASCADE
+
+
 	)
 
 	CREATE TABLE Request (
-		id int Primary Key,
+		id int identity ,
 		is_approved BIT,
-		game_id int FOREIGN KEY REFERENCES Game(id) ON DELETE CASCADE,
-		manager_id int FOREIGN KEY REFERENCES Manager(id),
-		representative_id int FOREIGN KEY REFERENCES Representative(id),
+		Match_id int ,
+		Stadium_Manager_id int ,
+		Club_Representative_id int ,
+
+		constraint Re_Pk primary key(id),
+		constraint Re_mi_Fk foreign key (Match_id) references  Match (id) on update cascade on delete cascade ,
+		constraint Re_cri_Fk foreign key (Club_Representative_id) references Club_Representative  (id)  on update cascade on delete cascade ,
+		constraint Re_smi_FK foreign key (Stadium_Manager_id) references Stadium_Manager (id) on update cascade on delete cascade ,
+	
+	)
+	
+	create table Ticket_purchase(
+		id int identity ,
+		Ticket_id int ,
+		Fan_id int ,
+
+		constraint Tp_Pk primary key(id),
+		constraint Tp_ti_Fk foreign key (Ticket_id) references  Ticket (id) on update cascade on delete cascade ,
+		constraint Tp_fi_Fk foreign key (Fan_id) references Fan  (id)  on update cascade on delete cascade 
 	);
 	GO
 
 CREATE PROC dropAllTables AS
 	DROP TABLE IF EXISTS Request, Ticket, System_Admin,
-	Representative, Sports_Association_Manager, Manager,
-	Fan, SystemUser, Game, Club, Stadium;
+	Club_Representative, Sports_Association_Manager, Stadium_Manager,
+	Fan, SystemUser, Match, Club, Stadium;
 GO
 
 
 CREATE PROC dropAllProceduresFunctionsViews AS
 	DROP PROC IF EXISTS createAllTables, dropAllTables, clearAllTables,
-	addAssociationManager, addNewMatch, deleteMatch, deleteMatchesOnStadium,
+	addAssociationStadium_Manager, addNewMatch, deleteMatch, deleteMatchesOnStadium,
 	addClub, addTicket, deleteClub, addStadium, deleteStadium, blockFan,
-	unblockFan, addRepresentative, addHostRequest, addStadiumManager,
+	unblockFan, addRepresentative, addHostRequest, addStadiumStadium_Manager,
 	acceptRequest, rejectRequest, addFan, purchaseTicket, updateMatchHost,
 	deleteMatchesOnStadium;
-	DROP VIEW IF EXISTS allAssocManagers, allClubRepresentatives,
-	allStadiumManagers, allFans, allMatches, allTickets, allCLubs,
+	DROP VIEW IF EXISTS allAssocStadium_Managers, allClubRepresentatives,
+	allStadiumStadium_Managers, allFans, allMatches, allTickets, allCLubs,
 	allStadiums, allRequests, clubsWithNoMatches,
 	matchesPerTeam, clubsNeverMatched;
 	DROP FUNCTION IF EXISTS allUnassignedMatches, viewAvailableStadiumsOn, 
@@ -122,30 +165,22 @@ CREATE PROC dropAllProceduresFunctionsViews AS
 	,clubsNeverPlayed, matchWithHighestAttendance, matchesRankedByAttendance
 	,requestsFromClub;
 	GO
-/*
-INSERT INTO SystemUser VALUES 
-('sss', 'ssss', 'sssss','ssssss');
-SELECT * FROM SystemUser;
-INSERT INTO Sports_Association_Manager VALUES
-	(7);
-SELECT * FROM Sports_Association_Manager;
-	*/
-GO
+
 CREATE PROC clearAllTables AS
-    DELETE FROM Request;
-	DELETE FROM System_Admin;
-	DELETE FROM Ticket;
-	DELETE FROM Sports_Association_Manager;
-	DELETE FROM Representative;
-	DELETE FROM Fan;
-	DELETE FROM Manager;
-	DELETE FROM SystemUser;
-	DELETE FROM Game;
-	DELETE FROM Club;
-	DELETE FROM Stadium;
+    TRUNCATE TABLE Request;
+	TRUNCATE TABLE System_Admin;
+	TRUNCATE TABLE Ticket;
+	TRUNCATE TABLE Sports_Association_Manager;
+	TRUNCATE TABLE Club_Representative;
+	TRUNCATE TABLE Fan;
+	TRUNCATE TABLE Stadium_Manager;
+	TRUNCATE TABLE SystemUser;
+	TRUNCATE TABLE Match;
+	TRUNCATE TABLE Club;
+	TRUNCATE TABLE Stadium;
 	GO
 
-CREATE VIEW allAssocManagers AS
+CREATE VIEW allAssocStadium_Managers AS
     SELECT SU.username, SU.uname
     FROM Sports_Association_Manager SAM
     JOIN SystemUser SU ON SU.id = SAM.id;
@@ -153,14 +188,14 @@ GO
 
 CREATE VIEW allClubRepresentatives AS
     SELECT su.username, su.uname, c.cname
-    FROM Representative R
+    FROM Club_Representative R
     JOIN SystemUser SU ON SU.id = R.id
 	JOIN Club C ON C.cname = r.cname;
 GO
 
-CREATE VIEW allStadiumManagers AS
+CREATE VIEW allStadiumStadium_Managers AS
     SELECT su.username, su.uname, S.sname
-    FROM Manager M
+    FROM Stadium_Manager M
     JOIN SystemUser SU ON SU.id = M.id
 	JOIN Stadium S ON S.sname = M.sname;
 GO
@@ -173,13 +208,13 @@ GO
 
 CREATE VIEW allMatches AS
     SELECT f_club, s_club, h_club, starting_time
-    FROM Game G
+    FROM Match G
 GO
 
 CREATE VIEW allTickets AS
     SELECT g.f_club, g.s_club, S.sname, g.starting_time
     FROM Ticket T
-	JOIN Game G ON T.game_id = G.id
+	JOIN Match G ON T.Match_id = G.id
 	JOIN Stadium S ON S.sname = G.sname;
 GO
 
@@ -196,17 +231,17 @@ GO
 CREATE VIEW allRequests AS
     SELECT SU1.uname User1, SU2.uname User2, RE.is_approved
     FROM Request Re
-	JOIN Representative R ON R.id = Re.id
+	JOIN Club_Representative R ON R.id = Re.id
 	JOIN SystemUser SU1 ON SU1.id = R.id
-	JOIN Manager M ON RE.manager_id = M.id
+	JOIN Stadium_Manager M ON RE.Stadium_Manager_id = M.id
 	JOIN SystemUser SU2 ON SU2.id = RE.id;
 GO
 
-CREATE PROCEDURE addAssociationManager 
+CREATE PROCEDURE addAssociationStadium_Manager 
 	@uname varchar(20), @username varchar(20), @password varchar(20) AS
 
 	INSERT INTO SystemUser VALUES 
-	(@uname, @username, @password);
+	(@uname, @username, @password, 'AStadium_Manager');
 	DECLARE @id int = (SELECT su.id FROM SystemUser su
 					WHERE su.uname = @uname AND su.username = @username);
     INSERT INTO Sports_Association_Manager VALUES
@@ -217,29 +252,29 @@ CREATE PROCEDURE addNewMatch
 	@f_club varchar(20), @s_club varchar(20), @h_club varchar(20),
 	@time datetime AS
 
-    INSERT INTO Game VALUES
+    INSERT INTO Match VALUES
 	(@f_club, @s_club, @h_club, @time, null);
 GO
 
 CREATE VIEW clubsWithNoMatches AS
     SELECT c.cname FROM Club C
 	EXCEPT
-	SELECT g1.f_club FROM Game g1
+	SELECT g1.f_club FROM Match g1
 	EXCEPT
-	SELECT g2.s_club FROM Game g2;
+	SELECT g2.s_club FROM Match g2;
 GO
 
 CREATE PROCEDURE deleteMatch 
 	@f_club varchar(20), @s_club varchar(20), @h_club varchar(20) AS
 
-    DELETE Game WHERE f_club = @f_club AND s_club = @s_club
+    DELETE Match WHERE f_club = @f_club AND s_club = @s_club
 	AND h_club = @h_club;
 GO
 
 CREATE PROCEDURE deleteMatchesOnStadium 
 	@stadium varchar(20) AS
 
-    DELETE FROM Game WHERE sname = @stadium;
+    DELETE FROM Match WHERE sname = @stadium;
 GO
 
 CREATE PROCEDURE addClub 
@@ -251,15 +286,15 @@ GO
 
 CREATE PROCEDURE addTicket 
 	@h_club varchar(20), @c_club varchar(20), @time datetime AS
-    DECLARE @game_id int = (SELECT id FROM Game 
+    DECLARE @Match_id int = (SELECT id FROM Match 
 					WHERE (h_club = @h_club) AND (f_club = @c_club OR s_club = @c_club));
-    INSERT INTO Ticket (game_id) VALUES 
-	(@game_id);
+    INSERT INTO Ticket (Match_id) VALUES 
+	(@Match_id);
 GO
 
 CREATE PROCEDURE deleteClub 
 	@cname varchar(20) AS
-        DELETE FROM Game WHERE f_club = @cname OR s_club = @cname;
+    
 	DELETE FROM Club WHERE cname = @cname;
 GO
 
@@ -295,10 +330,10 @@ CREATE PROCEDURE addRepresentative
 	@password varchar(20) AS
     
 	INSERT INTO SystemUser VALUES
-	(@uname, @username, @password);
+	(@uname, @username, @password, 'Club_Representative');
 	DECLARE @id int = (SELECT su.id FROM SystemUser su
 					WHERE su.uname = @uname AND su.username = @username);
-	INSERT INTO Representative VALUES
+	INSERT INTO Club_Representative VALUES
 	(@id, @cname);
 GO
 
@@ -311,7 +346,7 @@ CREATE FUNCTION viewAvailableStadiumsOn
 			FROM Stadium s
 			WHERE s.is_available = 1 
 			AND NOT s.sname IN (SELECT g.sname
-								FROM Game g	
+								FROM Match g	
 								WHERE s.sname = g.sname
 								AND g.starting_time = @time))
 GO
@@ -327,7 +362,7 @@ CREATE FUNCTION allUnassignedMatches AS
     FROM;
 GO
 
-CREATE PROCEDURE addStadiumManager AS
+CREATE PROCEDURE addStadiumStadium_Manager AS
     SELECT *
     FROM;
 GO
